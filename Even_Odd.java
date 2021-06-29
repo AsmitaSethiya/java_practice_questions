1f7d247f670fd694901857a7fc0e1911b81a7a9a
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-06-29 19:45:11 +0530

    Create Even_Odd.java

---
+++ Even_Odd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Even_Odd
+{
+	public static void main(String[] args)
+	{
+    		
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("Enter a number: ");
+        int	number  = in.nextInt();
+    	
+        if(number % 2 == 0)
+        {
+            System.out.println(number + "is a Even number");
+        }
+        else
+        {
+            System.out.println(number + " is a odd number");
+        }
+    	
+    	}
+}
