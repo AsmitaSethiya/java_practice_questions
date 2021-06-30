66fdb9958f375bb7e2e047bd702297ffacabe233
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-06-30 21:32:44 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,14 @@
+class Even
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Even No Between from 300 to 600 are: ");
+		for(int i = 300; i <= 600; i++)
+		{
+			if(i % 2 == 0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
