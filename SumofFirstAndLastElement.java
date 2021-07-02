3a7c8b3ddac967bee4cca1480f752c4dd6fb5169
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-02 21:35:06 +0530

    Create SumofFirstAndLastElement.java

---
+++ SumofFirstAndLastElement.java
@@ -0,0 +1,11 @@
+class SumofFirstAndLastElement
+{
+	public static void main(String[] args)
+	{
+		int a[] = {10, 20, 30, 40, 50};
+		
+		int sum = a[0] + a[4];
+
+		System.out.println(sum);	
+	}
+}
